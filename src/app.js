1daa00a1c1c6a675ed8c67bf44f9a9449d8ee734
Gia Thịnh <trangiathinh53@gmail.com>
2023-06-20 11:55:22 +0700

    Setup FE and Navbar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const app = express();
 
 const PORT = "6969";
 
-app.use(express.static("public"));
-app.use(express.static("views"));
+app.use('/public', express.static('public'));
+// app.use(express.static("views"));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/",function(req,res){
+    res.render("HomePage");
+})
