0e21020e3850f4dd82cd08337eae14f5cfa736ed
Khoa <ldkhoa21@clc.fitus.edu.vn>
2023-08-06 20:35:03 +0700

    update sidebar and new crud hospital v1.0

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -7,8 +7,4 @@ router.get('/dashboard', (req, res) => {
     res.render('Dashboard');
 });
 
-router.get("/CRUDAccount", accountController.listAccount);
-router.get('/addAccount', accountController.addAccount);
-router.post('/addAccount', accountController.postAccount);
-
 module.exports = router;
