8813905423e1f1a2194fa53fdc4288a872b085a8
kreema1602 <100921862+kreema1602@users.noreply.github.com>
2023-08-07 15:54:26 +0700

    createAccount ><

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,18 +2,17 @@ const Account = require("../models/accounts")
 
 exports.listAccount = async(req, res) =>{
     const account = await Account.find()
-    res.render('CRUDAccount', {account})
+    res.render('account', {account})
 }
 
 // New acocunt
 exports.createAccount = async(req, res) =>{
-    // const newAcc = new Account({
-    //     username: req.body.username,
-    //     password: req.body.password,
-    //     email: req.body.email,
-    //     role: req.body.role
-    // })
-    // newAcc.save()
-    console.log(req.body.username)
-    res.send('done')
+    const newAcc = new Account({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        role: req.body.role
+    })
+    newAcc.save()
+    res.redirect('/admin/account')
 }
--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -6,12 +6,12 @@ router.get('/dashboard', (req, res) => {
     res.render('Dashboard');
 });
 
-router.get("/CRUDAccount", accountController.listAccount);
+router.get("/account", accountController.listAccount);
 
-router.get('/add', (req, res) =>{
+router.get('/account/add', (req, res) =>{
     res.render('addAccount')
 })
 
-router.post('/add', accountController.createAccount);
+router.post('/account/add', accountController.createAccount);
 
 module.exports = router;
