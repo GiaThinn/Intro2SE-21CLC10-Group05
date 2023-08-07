e0b80d38bd04155b6bde9c2d1dfcb7b6b85153e5
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2023-08-07 12:09:12 +0700

    body-parser adding

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const hospitalRoute = require("./routes/hospitalRoute");
 const patientRoute = require("./routes/patientRoute");
 const sessionHandler = require("./middleware/session");
 const auth = require("./middleware/authentication");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use('/public', express.static('public'));
 // app.use(express.static("views"));
 app.set('view engine', 'ejs');
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 const mongoURI = `mongodb+srv://thieuquanlac:iMyQSZ5ZQLsYnJMb@cluster0.eezcbqy.mongodb.net/HealthUs?retryWrites=true&w=majority`
