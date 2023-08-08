f7da6a731022a09dcd43a6464a7a0c3a668f8033
Khoa <ldkhoa21@clc.fitus.edu.vn>
2023-08-08 20:44:22 +0700

    create admin files

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const accountController = require('../controllers/accountController')
 
 router.get('/', (req, res) => {
-    res.render('Dashboard');
+    res.render('admin_dashboard');
 });
 
 router.get("/account", accountController.listAccount);
