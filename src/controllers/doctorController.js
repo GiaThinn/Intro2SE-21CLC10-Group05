6d8c68d2861cedacd185e25b0a178d15b47e14c4
Khoa <ldkhoa21@clc.fitus.edu.vn>
2023-08-08 21:50:34 +0700

    admin hospital crud doctor

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,58 @@
+const Doctor = require("../models/doctors")
+
+exports.listDoctor = async (req, res) => {
+    const doctor = await Doctor.find()
+    res.render('doctor', {doctor})
+}
+
+exports.createDoctor = async(req, res) =>{
+    await Doctor.create({
+        doctorID: req.body.doctorID,
+        name: req.body.name,
+        hospitalID: req.body.hospitalID,
+        specialist: req.body.specialist,
+        phone: req.body.phone,
+        birth: req.body.birth,
+        gender: req.body.gender,
+        email: req.body.email,
+        yearExp: req.body.yearExp,
+        description: req.body.description,
+        avatar: req.body.avatar,
+        schedule: req.body.schedule,
+    });
+    res.redirect('/admin/doctor')
+}
+
+exports.updateDoctor = async(req, res) => {
+    try{
+        const doctor = await Doctor.findOne({_id: req.params.id})
+        res.render('updateDoctor', {doctor})
+    } catch (error){console.log(error)}
+}
+
+exports.updateDoctorPost = async(req, res) => {
+    try{
+        await Doctor.findByIdAndUpdate(req.params.id,{
+            doctorID: req.body.doctorID,
+            name: req.body.name,
+            hospitalID: req.body.hospitalID,
+            specialist: req.body.specialist,
+            phone: req.body.phone,
+            birth: req.body.birth,
+            gender: req.body.gender,
+            email: req.body.email,
+            yearExp: req.body.yearExp,
+            description: req.body.description,
+            avatar: req.body.avatar,
+            schedule: req.body.schedule,
+        })
+        res.redirect('/admin/doctor')
+    } catch{}
+}
+
+exports.deleteDoctor = async(req, res) => {
+    try{
+        await Doctor.deleteOne({_id: req.params.id});
+        res.redirect('/admin/doctor')
+    } catch(error){}
+}
--- src/routes/hospitalRoute.js
+++ src/routes/hospitalRoute.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const doctorController = require('../controllers/doctorController')
 router.get('/', (req, res) => {
     res.render('hospital_dashboard');
 });
+router.get("/doctor", doctorController.listDoctor);
+
+router.get('/doctor/add', (req, res) =>{
+    res.render('adddoctor')
+})
+router.post('/doctor/add', doctorController.createDoctor);
+
+router.get('/doctor/update/:id', doctorController.updateDoctor)
+
+router.put('/doctor/update/:id', doctorController.updateDoctorPost)
+
+router.delete('/doctor/delete/:id',doctorController.deleteDoctor)
 module.exports = router;
